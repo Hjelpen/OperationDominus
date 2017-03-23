e6ead39197c6404202f2612575b39e8dcd90439b
Eric <Hjelpenlindros@gmail.com>
2017-03-23 19:22:07 +0100

    Startup tillägg

--- AngularJS.Journey.Skolprojekt/App_Start/Startup.cs
+++ AngularJS.Journey.Skolprojekt/App_Start/Startup.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using AngularJS.Journey.Skolprojekt.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System.Web.Http;
-
+using System;
 
 [assembly: OwinStartup(typeof(AngularJS.Journey.Skolprojekt.App_Start.Startup))]
 namespace AngularJS.Journey.Skolprojekt.App_Start
@@ -20,8 +22,16 @@ namespace AngularJS.Journey.Skolprojekt.App_Start
 
         public void ConfigureOAuth(IAppBuilder app)
         {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                Provider = new AuthorizationServerProvider(),
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(10)
+            };
 
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
-
     }
 }
