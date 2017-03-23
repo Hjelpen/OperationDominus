88f0c09eb54632c0696a1d9645afdd5d80a5cea5
Eric <Hjelpenlindros@gmail.com>
2017-03-23 17:56:51 +0100

    Startup
    Konfig för start av appen för att hantera tokens och autentisering.

--- AngularJS.Journey.Skolprojekt/App_Start/Startup.cs
+++ AngularJS.Journey.Skolprojekt/App_Start/Startup.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
 
+
+[assembly: OwinStartup(typeof(AngularJS.Journey.Skolprojekt.App_Start.Startup))]
 namespace AngularJS.Journey.Skolprojekt.App_Start
 {
     public class Startup
-    {
+    {      
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+
+        }
+
     }
 }
