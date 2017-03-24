c1f8e369714c08894367fca03956deddcdcc5822
Eric <Hjelpenlindros@gmail.com>
2017-03-24 21:56:29 +0100

    AuthServerProvider
    validerar token förfrågan mot användare/lösenord, skickar tillbaka en
    token eller felmeddelande.

---
+++ AngularJS.Journey.Skolprojekt/Providers/AuthorizationServerProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System.Threading.Tasks;
+using AngularJS.Journey.Skolprojekt.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+
+namespace AngularJS.Journey.Skolprojekt.Providers
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext contex)
+        {
+            contex.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext contex)
+        {
+            contex.OwinContext.Response.Headers.Add("Acess-Control-Allow-Origin", new[] { "*" });
+
+            using (UserRepository _repo = new UserRepository())
+            {
+                IdentityUser user = await _repo.FindUser(contex.UserName, contex.Password);
+
+                if (user == null)
+                {
+                    contex.SetError("invalid_grant", "Felaktigt användarnamn eller lösenord.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(contex.Options.AuthenticationType);
+            identity.AddClaim(new Claim("user_name", contex.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+            contex.Validated(identity);
+        }
+
+    }
+}
