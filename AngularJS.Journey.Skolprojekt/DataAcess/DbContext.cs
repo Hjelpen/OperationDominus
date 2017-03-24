2ef3d70caae47e492b96c7467ea3c4504135a249
Eric <Hjelpenlindros@gmail.com>
2017-03-24 16:29:04 +0100

    Dbcontext

---
+++ AngularJS.Journey.Skolprojekt/DataAcess/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJS.Journey.Skolprojekt.DataAcess
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+        public DbContext() : base("DbContext")
+        {
+
+        }
+    }
+}
