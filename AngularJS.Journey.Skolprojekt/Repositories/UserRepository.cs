25c513e2a58eab24dc67f07313bf2c6922813cf0
Eric <Hjelpenlindros@gmail.com>
2017-03-24 22:08:46 +0100

    UserRepo
    Ett repo för att skapa och hitta användare, kommer utöka senare.

---
+++ AngularJS.Journey.Skolprojekt/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
+using AngularJS.Journey.Skolprojekt.Models;
+using AngularJS.Journey.Skolprojekt.DataAcess;
+
+namespace AngularJS.Journey.Skolprojekt.Repositories
+{
+    public class UserRepository : IDisposable
+    {
+       private DbContext _ctx;
+
+       private UserManager<IdentityUser> _userManager;
+
+        public UserRepository()
+        {
+            _ctx = new DbContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser (User userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+            _userManager.Dispose();
+        }
+    }
+}
