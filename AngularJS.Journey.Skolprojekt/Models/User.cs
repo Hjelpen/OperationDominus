11147e24476d1589c49ea402c29b5afd3bb7bb62
Eric <Hjelpenlindros@gmail.com>
2017-03-24 15:40:54 +0100

    Users
    en model för registering av ny användare

---
+++ AngularJS.Journey.Skolprojekt/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AngularJS.Journey.Skolprojekt.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "Användarnamn")]
+
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Lösenordet {0} måste bestå av minst {2} tecken.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Lösenord")]
+
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Bekräfta lösenord")]
+        [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
