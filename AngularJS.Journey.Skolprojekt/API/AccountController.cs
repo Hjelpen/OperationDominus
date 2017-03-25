bf59c9218109654961adb02766f971f47dffdd47
Eric <Hjelpenlindros@gmail.com>
2017-03-25 16:49:38 +0100

    AccountController
    Ett AccountController API som hjälper till med att skapa upp nya konton
    med UserRepoistory

---
+++ AngularJS.Journey.Skolprojekt/API/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using AngularJS.Journey.Skolprojekt.Models;
+using AngularJS.Journey.Skolprojekt.Repositories;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AngularJS.Journey.Skolprojekt.API
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private UserRepository _repo = null;
+
+        public AccountController()
+        {
+            _repo = new UserRepository();
+        }
+
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register (User userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await _repo.RegisterUser(userModel);
+
+            if (result == null) return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
