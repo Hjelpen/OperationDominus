3f477e6891687d9697bfedfb9c5d803e72fac6f6
Eric <Hjelpenlindros@gmail.com>
2017-03-31 21:45:58 +0200

    Registrera ny användare 1.0
    Möjlighet att registrera en ny användare via appens gränssnitt finns nu.

--- AngularJS.Journey.Skolprojekt/App/Controllers/registerController.js
+++ AngularJS.Journey.Skolprojekt/App/Controllers/registerController.js
@@ -1,7 +1,43 @@
 ﻿(function () {
     "use strict";
     angular.module('AngularJourneyApp') 
-        .controller('registerController', ['$scope', '$location', function ($scope, $location) {
+        .controller('registerController', ['$scope', '$location', '$timeout', 'registerService', function ($scope, $location, $timeout, registerService) {
+
+            $scope.savedSuccessfully = false;
+            $scope.message = "";
+
+            $scope.registration = {
+                userName: "",
+                password: "",
+                confirmPassword: ""
+            };
+
+            $scope.register = function () {
+
+                registerService.saveRegistration($scope.registration).then(function (response) {
+
+                    $scope.savedSuccessfully = true;
+                    $scope.message = "användare " + $scope.registration.userName + " har blivit registrerad, omredigeras till login sidan om 3 sekunder.";
+                    startTimer();
+
+                },
+                 function (response) {
+                     var errors = [];
+                     for (var key in response.data.modelState) {
+                         for (var i = 0; i < response.data.modelState[key].length; i++) {
+                             errors.push(response.data.modelState[key][i]);
+                         }
+                     }
+                     $scope.message = "Misslyckades att registrera ny användare på grund av:" + errors.join(' ');
+                 });
+            };
+
+            var startTimer = function () {
+                var timer = $timeout(function () {
+                    $timeout.cancel(timer);
+                    $location.path('/home');
+                }, 3000);
+            }
 
         }]);
 })();
---
+++ AngularJS.Journey.Skolprojekt/App/Services/registerService.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    "use strict";
+    angular.module('AngularJourneyApp')
+       .factory('registerService', ['$http', function ($http) {
+
+           var registerServiceFactory = {};
+
+           var _saveRegistration = function (registration) {
+
+               return $http.post('https://localhost:44341/api/account/register', registration)
+                   .then(function (response) {
+                   return response;
+               });
+           };
+
+           registerServiceFactory.saveRegistration = _saveRegistration;
+           return registerServiceFactory;
+
+       }]);
+})();
